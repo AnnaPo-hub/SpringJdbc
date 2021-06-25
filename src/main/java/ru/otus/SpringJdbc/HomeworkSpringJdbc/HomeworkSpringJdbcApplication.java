b19fd1cb6e0a1b53b0d97deb11cd0dcc4fa92b8a
Anna Popova <anna.popova201909@gmail.com>
2021-06-25 13:26:19 +0300

    initial commit

---
+++ src/main/java/ru/otus/SpringJdbc/HomeworkSpringJdbc/HomeworkSpringJdbcApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.SpringJdbc.HomeworkSpringJdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkSpringJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkSpringJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/otus/SpringJdbc/HomeworkSpringJdbc/HomeworkSpringJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.SpringJdbc.HomeworkSpringJdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkSpringJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
