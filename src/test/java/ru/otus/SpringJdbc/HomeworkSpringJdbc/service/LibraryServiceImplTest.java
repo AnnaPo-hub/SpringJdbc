ac9236419e210acddf3d22c4c92b79e4cc796b88
Anna Popova <anna.popova201909@gmail.com>
2021-06-29 23:44:09 +0300

    added tests

--- src/test/java/ru/otus/SpringJdbc/HomeworkSpringJdbc/service/LibraryServiceImplTest.java
+++ src/test/java/ru/otus/SpringJdbc/HomeworkSpringJdbc/service/LibraryServiceImplTest.java
@@ -3,16 +3,21 @@ package ru.otus.SpringJdbc.HomeworkSpringJdbc.service;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
 import ru.otus.SpringJdbc.HomeworkSpringJdbc.dao.LibraryDaoJdbc;
 import ru.otus.SpringJdbc.HomeworkSpringJdbc.domain.Author;
 import ru.otus.SpringJdbc.HomeworkSpringJdbc.domain.Book;
 import ru.otus.SpringJdbc.HomeworkSpringJdbc.domain.Genre;
 
-@SpringBootTest(classes = {LibraryServiceImpl.class, LibraryDaoJdbc.class,})
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@JdbcTest
+@Import({LibraryDaoJdbc.class, LibraryServiceImpl.class})
 class LibraryServiceImplTest {
 
     Book bookToAdd = new Book((long) 3, "The poems", new Author((long) 1, "Block"), new Genre((long) 1, "Poetry"));
+    Book existingBook = new Book((long) 2, "The lady unknown", new Author((long) 1, "Block"), new Genre((long) 1, "Poetry"));
     @Autowired
     LibraryService libraryService;
 
@@ -24,11 +29,12 @@ class LibraryServiceImplTest {
 
     @Test
     void shouldShowAllBooks() {
-        Assertions.assertEquals(3, libraryService.showAllBooks().size());
+        Assertions.assertEquals(2, libraryService.showAllBooks().size());
     }
 
     @Test
-    void shouldFindBookByName() {
+    void shouldFindBookByName() {// сравнить, что полученный по поиску объем и есть тот, который нужен
+        assertTrue(libraryService.findBookByName("The lady unknown").contains(existingBook));
     }
 
     @Test
