547df1f46013ee10ec1f9822338d6fa89f161500
Anna Popova <anna.popova201909@gmail.com>
2021-08-23 13:23:02 +0300

    удалила сервисный слой

--- src/main/java/ru/otus/SpringJdbc/HomeworkSpringJdbc/HomeworkSpringApp.java
+++ src/main/java/ru/otus/SpringJdbc/HomeworkSpringJdbc/HomeworkSpringApp.java
@@ -5,60 +5,35 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.reactive.config.EnableWebFlux;
 import ru.otus.SpringJdbc.HomeworkSpringJdbc.domain.Author;
 import ru.otus.SpringJdbc.HomeworkSpringJdbc.domain.Book;
 import ru.otus.SpringJdbc.HomeworkSpringJdbc.domain.Comment;
 import ru.otus.SpringJdbc.HomeworkSpringJdbc.domain.Genre;
-import ru.otus.SpringJdbc.HomeworkSpringJdbc.service.AuthorService;
-import ru.otus.SpringJdbc.HomeworkSpringJdbc.service.BookService;
-import ru.otus.SpringJdbc.HomeworkSpringJdbc.service.CommentService;
-import ru.otus.SpringJdbc.HomeworkSpringJdbc.service.GenreService;
+
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @EnableMongoRepositories
 @SpringBootApplication
+@EnableWebFlux
 public class HomeworkSpringApp {
 
     public static void main(String[] args) {
         final ConfigurableApplicationContext run = SpringApplication.run(HomeworkSpringApp.class, args);
-        final AuthorService authorService = run.getBean(AuthorService.class);
 
-        final Author pushkin = authorService.insert(new Author((long) 1, "Pushkin"));
+        final Author pushkin = .insert(new Author((long) 1, "Pushkin"));
         System.out.println("добавила аутхора " + pushkin);
 
         final Author proust = authorService.insert(new Author((long) 2, "Proust"));
         System.out.println("добавила аутхора Пруста " + proust);
 
-        final List<Author> pushkin1 = authorService.getByName("Pushkin");
-        System.out.println("нашла аутхора Пушкина " + pushkin1);
-
-        final Optional<Author> authorById = authorService.getById((long) 2);
-        System.out.println("нашла аутхора Пруста по id " + authorById);
-
-        // authorService.deleteById((long)1);
-
-        final List<Author> allAuthors = authorService.getAll();
-        System.out.println("все аутхоры, должен остаться с id 2  : " + allAuthors);
 
 
         System.out.println("--------GenreService-------");
-        final GenreService genreService = run.getBean(GenreService.class);
         final Genre fiction = genreService.insert(new Genre((long) 1, "Fiction"));
         final Genre poetry = genreService.insert(new Genre((long) 2, "Poetry"));
-        final List<Genre> allGenres = genreService.getAll();
-        System.out.println("all genres: " + allGenres);
-        final Optional<Genre> genreById = genreService.getById((long) 1);
-        System.out.println("found genre by id 1 -  Fiction " + genreById);
-
-        final List<Genre> fiction1 = genreService.getByName("Fiction");
-        System.out.println("getByName, should be Fiction " + fiction1);
-        // genreService.deleteById((long)2);
-
-        final List<Genre> allGenres2 = genreService.getAll();
-        System.out.println("all genres, should be 1   with id 1 " + allGenres2);
 
         System.out.println("---------BookService-----------------------------");
 
