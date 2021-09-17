33afe98c58fcd94d82649620635ff83d7a9e8e8a
Anna Popova <anna.popova201909@gmail.com>
2021-09-17 16:16:16 +0300

    initial  commit

---
+++ src/main/java/ru/otus/SpringJdbc/HomeworkSpringJdbc/config/BatchConfig.java
@@ -0,0 +1,9 @@
+package ru.otus.SpringJdbc.HomeworkSpringJdbc.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+public class BatchConfig {
+}
