89aff77ef299ca5d53fad9b6ceffcb79a383f339
Anna Popova <anna.popova201909@gmail.com>
2021-09-17 17:23:39 +0300

    added SpringBatch configuration

--- src/main/java/ru/otus/SpringJdbc/HomeworkSpringJdbc/config/BatchConfiguration.java
+++ src/main/java/ru/otus/SpringJdbc/HomeworkSpringJdbc/config/BatchConfiguration.java
@@ -1,48 +1,64 @@
 package ru.otus.SpringJdbc.HomeworkSpringJdbc.config;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.data.MongoItemWriter;
 import org.springframework.batch.item.data.builder.MongoItemWriterBuilder;
 import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import ru.otus.SpringJdbc.HomeworkSpringJdbc.domain.Book;
 
+import javax.persistence.EntityManagerFactory;
+
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
+    private static final int CHUNK_SIZE = 2;
     @Autowired
     private JobBuilderFactory jobBuilderFactory;
 
     @Autowired
     private StepBuilderFactory stepBuilderFactory;
 
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
+
     @Bean
-    public ItemReader<Book> reader() throws Exception {
-        String jpqlQuery = "select b from Book b ";
-
-        JpaPagingItemReader<Book> reader = new JpaPagingItemReader<Book>();
-        reader.setQueryString(jpqlQuery);
-      //  reader.setEntityManagerFactory();
-        reader.setPageSize(3);
-        reader.afterPropertiesSet();
-        reader.setSaveState(true);
-
-        return reader;
+    public JpaPagingItemReader<Book> reader() {
+        return new JpaPagingItemReaderBuilder<Book>().name("jpaPagingItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(CHUNK_SIZE)
+                .queryString("select b from Book b ")
+                .build();
     }
 
     @Bean
-    public MongoItemWriter<Book> writer (MongoTemplate mongoTemplate){
+    public MongoItemWriter<Book> writer(MongoTemplate mongoTemplate) {
         return new MongoItemWriterBuilder<Book>().template(mongoTemplate).collection("book").build();
     }
 
-//    @Bean
-//    public BookItemProcessor processor(){
-//        return new BookItemProcessor();
-//    }
+    @Bean
+    public Job bookJob() {
+        return jobBuilderFactory.get("bookJob")
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .<Book, Book>chunk(CHUNK_SIZE)
+                .reader(reader())
+                .writer(writer())
+                .build();
+    }
 }
